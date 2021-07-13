a0d5f9a4b7a384b1bea67fcda457fab91c02582b
jcleonel <jean.leonel.costa@gmail.com>
2021-07-13 18:54:08 -0300

    Criando projeto backend e frontend

---
+++ backend/src/main/java/br/com/bluetask/app/BluetaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.bluetask.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluetaskApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/bluetask/app/BluetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bluetask.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
