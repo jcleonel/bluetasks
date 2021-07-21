bfb9354ee4ea34007c893488b00c9233e5bb6869
jcleonel <jean.leonel.costa@gmail.com>
2021-07-21 12:23:39 -0300

    Ajustando repositorios via web services

--- backend/src/main/java/br/com/bluetask/app/BluetaskApplication.java
+++ backend/src/main/java/br/com/bluetask/app/BluetaskApplication.java
@@ -2,12 +2,22 @@ package br.com.bluetask.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import br.com.bluetask.app.domain.task.Task;
 
 @SpringBootApplication
-public class BluetaskApplication {
+public class BluetaskApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluetaskApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
--- backend/src/main/java/br/com/bluetask/app/domain/user/AppUserRepository.java
+++ backend/src/main/java/br/com/bluetask/app/domain/user/AppUserRepository.java
@@ -1,9 +1,11 @@
 package br.com.bluetask.app.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
 	
 	AppUser findByUsername(String username);
