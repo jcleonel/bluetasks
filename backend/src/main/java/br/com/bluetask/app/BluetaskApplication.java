04225d1e7189a56ba87eb20492bf8279003423fd
jcleonel <jean.leonel.costa@gmail.com>
2021-07-28 16:48:35 -0300

    Configurando os Logs

--- backend/src/main/java/br/com/bluetask/app/BluetaskApplication.java
+++ backend/src/main/java/br/com/bluetask/app/BluetaskApplication.java
@@ -1,5 +1,7 @@
 package br.com.bluetask.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,8 @@ import br.com.bluetask.app.domain.task.Task;
 @SpringBootApplication
 public class BluetaskApplication implements RepositoryRestConfigurer {
 
+	private static Logger logger = LoggerFactory.getLogger(BluetaskApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BluetaskApplication.class, args);
 	}
