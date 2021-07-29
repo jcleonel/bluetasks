7308316f7dd3c4dc0d1a9aa6871e3836cf71fbbb
jcleonel <jean.leonel.costa@gmail.com>
2021-07-29 18:44:46 -0300

    Criando o AuthenticationFilter

---
+++ backend/src/main/java/br/com/bluetask/app/infrastructure/web/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package br.com.bluetask.app.infrastructure.web.security;
+
+public class SecurityConstants {
+
+	public static final long EXPIRATION_TIME = 86400000;
+	public static final String SECRET_KEY = "tHeSeCrEtKey";
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String TOKEN_PREFIX = "Bearer ";
+}
