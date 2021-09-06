59aec30d3ca1a5383bf61ac93466c134d317a1b4
jcleonel <jean.leonel.costa@gmail.com>
2021-09-06 08:25:45 -0300

    implementando hook de autenticacao

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,39 +1,31 @@
-import { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import TaskForm from "./components/TaskForm";
-import TaskListTable from "./components/TaskListTable";
-import Login from "./components/Login";
+import React from 'react';
+import NavBar from './components/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import TaskListTable from './components/TaskListTable';
+import TaskForm from './components/TaskForm';
+import Login from './components/Login';
+import { useAuth, AuthContext } from './hooks/useAuth'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = (props) => {
+  const auth = useAuth();
 
-    this.onRefreshHandler = this.onRefreshHandler.bind(this);
-  }
-
-onRefreshHandler() {
-  this.forceUpdate();
-}
-
-  render(){
-    return (
+  return (
+    <AuthContext.Provider value={auth}>
       <BrowserRouter>
-        <div>
-          <NavBar onLinkClick={this.onRefreshHandler} />
-          <div className="container" style={{ marginTop: 20}}>
-          <Switch>
-            <Route exact path="/login" render={() => <Login onLoginSuccess={this.onRefreshHandler} />} />
-            <Route exact path="/" component={TaskListTable} /> 
-            <Route exact path="/form" component={TaskForm} />
-            <Route exact path="/form/:id" component={TaskForm} />
-          </Switch>
-          </div>          
+        <div className="App">
+          <NavBar />
+          <div className="container" style={{ marginTop: 20 }}>
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/form" component={TaskForm} />
+              <Route exact path="/form/:id" component={TaskForm} />
+              <Route path="/" component={TaskListTable} />
+            </Switch>
+          </div>
         </div>
       </BrowserRouter>
-    );
-  }
-
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
